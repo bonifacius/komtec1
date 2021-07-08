51d8cf9dc587d69ffadad26ec8e7adf5e30e8789
Roman Basmanov <basmanovroman@mail.ru>
2021-07-08 14:58:29 +0500

    add validation unique_together in serializer.py

--- api/core/serializers.py
+++ api/core/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework.validators import UniqueTogetherValidator
 
 from core.models import Catalog, CatalogItem
 
@@ -7,12 +8,24 @@ class CatalogSerializer(ModelSerializer):
     class Meta:
         model = Catalog
         fields = '__all__'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Catalog.objects.all(),
+                fields=['name', 'version']
+            )
+        ]
 
 
 class CatalogItemSerializer(ModelSerializer):
     class Meta:
         model = CatalogItem
         fields = '__all__'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=CatalogItem.objects.all(),
+                fields=['code', 'name_item_catalog']
+            )
+        ]
 
 
 class CatalogDateSerializer(ModelSerializer):
